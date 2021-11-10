ea95272462405fb0460dfc7c028169166ed5c7b8
liulihaocai <liulihaocaiqwq@gmail.com>
2021-11-10 19:18:25 +0800

    better protocol to send messages

--- src/main/java/me/liuli/luminous/loader/connect/MessageThread.kt
+++ src/main/java/me/liuli/luminous/loader/connect/MessageThread.kt
@@ -7,13 +7,18 @@ import java.io.File
 import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
+import java.nio.charset.StandardCharsets
 
 /**
  * send messages through UDP can be used to communicate with loader and agent without SYN/ACK handshake and keep alive
  */
 class MessageThread : Thread() {
     private val socket = DatagramSocket()
-    private val buf = ByteArray(1024) // max size of a UDP packet
+
+    /**
+     * cache for smart packet size protocol, -1 for next packet is size packet
+     */
+    private var nextSize = -1
 
     init {
         File(Luminous.cacheDir, "SOCKET_${if(Luminous.isAgent) {"CLIENT"} else {"SERVER"}}_PORT").writeBytes(socket.localPort.toString().toByteArray())
@@ -21,9 +26,21 @@ class MessageThread : Thread() {
 
     override fun run() {
         while (true) {
+            val buf = ByteArray(if(nextSize == -1) { 32 } else { nextSize })
             val packet = DatagramPacket(buf, buf.size)
             socket.receive(packet)
-            parse(String(packet.data, 0, packet.length))
+            val packetStr = packet.data.toString(StandardCharsets.UTF_8)
+            try {
+                nextSize = if(nextSize == -1) {
+                    packetStr.split("|")[0].toInt()
+                } else {
+                    parse(packetStr)
+                    // next packet must be size packet
+                    -1
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
     }
 
@@ -40,9 +57,15 @@ class MessageThread : Thread() {
     }
 
     fun send(msg: Message) {
-        val json = Klaxon().toJsonString(msg)
-        val packet = DatagramPacket(json.toByteArray(), json.length, InetAddress.getLocalHost(),
-            File(Luminous.cacheDir, "SOCKET_${if(Luminous.isAgent) {"SERVER"} else {"CLIENT"}}_PORT").readText().toInt())
+        val json = Klaxon().toJsonString(msg).toByteArray()
+        val host = InetAddress.getLocalHost()
+        val port = File(Luminous.cacheDir, "SOCKET_${if(Luminous.isAgent) {"SERVER"} else {"CLIENT"}}_PORT").readText().toInt()
+        json.size.also {
+            val buf = "$it|".toByteArray()
+            val packet = DatagramPacket(buf, buf.size, host, port)
+            socket.send(packet)
+        }
+        val packet = DatagramPacket(json, json.size, host, port)
         socket.send(packet)
     }
 }
